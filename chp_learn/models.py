70455cd2803d5f054f902930098f57cff0298308
Chase Yakaboski <chase.a.yakaboski@dartmouth.edu>
2022-03-10 07:39:59 -0500

    Changed model field relation to predicate.

--- chp_learn/models.py
+++ chp_learn/models.py
@@ -21,7 +21,7 @@ class GeneToFillGeneResult(models.Model):
     fill_gene = models.ForeignKey('Gene', on_delete=models.CASCADE, related_name='g2g_fill_gene')
     query_gene = models.ForeignKey('Gene', on_delete=models.CASCADE, related_name='g2g_query_gene')
     weight = models.FloatField()
-    relation = models.CharField(max_length=128)
+    predicate = models.CharField(max_length=128)
 
     def __str__(self):
         return f'{self.query_gene} -> {self.relation} -> {self.fill_gene} with weight: {self.weight}'
@@ -35,7 +35,7 @@ class DiseaseToFillGeneResult(models.Model):
     fill_gene = models.ForeignKey('Gene', on_delete=models.CASCADE, related_name='g2dis_fill_gene')
     query_disease = models.ForeignKey('Disease', on_delete=models.CASCADE, related_name='g2dis_query_disease')
     weight = models.FloatField()
-    relation = models.CharField(max_length=128)
+    predicate = models.CharField(max_length=128)
 
     def __str__(self):
         return f'{self.query_disease} -> {self.relation} -> {self.fill_gene} with weight: {self.weight}'
